e0163b7a81499ae8a4d29352f28f89c0e652255b
paulo <pauloalegre0207@gmail.com>
2024-01-09 20:30:49 +0800

    first commit

---
+++ jenkins-ec2-terraform/main.tf
@@ -0,0 +1,122 @@
+resource "aws_iam_role" "jenkins_terraform_role" {
+  name = "jenkins-terraform"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "jenkins_terraform_attachment" {
+  role       = aws_iam_role.jenkins_terraform_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "jenkins_terraform_profile" {
+  name = "Jenkins-terraform"
+  role = aws_iam_role.jenkins_terraform_role.name
+}
+
+
+resource "aws_security_group" "jenkins-sg" {
+  name        = "Jenkins-Security Group"
+  description = "Open 22,443,80,8080,9000,3000"
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+resource "aws_security_group" "splunk-sg" {
+  name        = "Splunk-Security Group"
+  description = "Open 22,8000"
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 8000, 8088] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "splunk-sg"
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami                    = "ami-0fa377108253bf620"
+  instance_type          = "t2.large"
+  key_name               = "ipau"
+  vpc_security_group_ids = [aws_security_group.jenkins-sg.id]
+  user_data              = templatefile("./install_jenkins.sh", {})
+  iam_instance_profile   = aws_iam_instance_profile.jenkins_terraform_profile.name
+
+  tags = {
+    Name = "jenkins"
+  }
+
+  root_block_device {
+    volume_size = 30
+  }
+}
+
+
+
+# EC2 Instance (Medium)
+resource "aws_instance" "splunk" {
+  ami           = "ami-0fa377108253bf620"
+  instance_type = "t2.medium"
+  key_name      = "ipau"
+  vpc_security_group_ids = [aws_security_group.splunk-sg.id]
+  iam_instance_profile = aws_iam_instance_profile.jenkins_terraform_profile.name
+
+  root_block_device {
+    volume_size = 24
+  }
+}
